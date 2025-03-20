ff5386610a7f2201ecbe04b6b49929d600a61cb4
Yogi2084 <1si22is406@sit.ac.in>
2025-03-20 14:41:43 +0530

    add Restaurant, MenuItem, Order, and OrderItem models with relationships and status enum

---
+++ prisma/migrations/20250320091114_v5/migration.sql
@@ -0,0 +1,59 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('PLACED', 'PREPARING', 'COMPLETED', 'CANCELLED');
+
+-- CreateTable
+CREATE TABLE "Restaurant" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+
+    CONSTRAINT "Restaurant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MenuItem" (
+    "id" SERIAL NOT NULL,
+    "restaurantId" INTEGER NOT NULL,
+    "namE" TEXT NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "isAvailable" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "MenuItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" INTEGER NOT NULL,
+    "customerId" INTEGER NOT NULL,
+    "restaurantId" INTEGER NOT NULL,
+    "status" "Status" NOT NULL,
+    "totalPrice" DECIMAL(65,30) NOT NULL,
+    "orderTime" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "id" INTEGER NOT NULL,
+    "orderId" INTEGER NOT NULL,
+    "menuItemId" INTEGER NOT NULL,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Restaurant_name_key" ON "Restaurant"("name");
+
+-- AddForeignKey
+ALTER TABLE "MenuItem" ADD CONSTRAINT "MenuItem_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_restaurantId_fkey" FOREIGN KEY ("restaurantId") REFERENCES "Restaurant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_menuItemId_fkey" FOREIGN KEY ("menuItemId") REFERENCES "MenuItem"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
